e2a4de220f3efe038242765a6e50cab6a9c09729
Andrea Malnati <a.malnati3@campus.unimib.it>
2024-03-27 18:27:24 +0100

    Aggiunta server Spring locale e dipendenze

---
+++ src/main/java/org/ssv/service/SpringConfig.java
@@ -0,0 +1,29 @@
+package org.ssv.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final Environment env;
+
+    @Autowired
+    public SpringConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String urls = env.getProperty("cors.urls");
+        if (urls != null) {
+            CorsRegistration reg = registry.addMapping("/*");
+            for (String url : urls.split(",")) {
+                reg.allowedOrigins(url).allowedMethods("*");
+            }
+        }
+    }
+}
