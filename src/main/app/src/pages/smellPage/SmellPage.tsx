57b2da70449efa8ad65bb4e230058447348e5ae0
Roberto Negro <r.negro3@campus.unimib.it>
2024-04-23 14:00:11 +0200

    SmellPage implementation to finish

--- src/main/app/src/pages/smellPage/SmellPage.tsx
+++ src/main/app/src/pages/smellPage/SmellPage.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import TopBar from '../../components/topBar/TopBar';
 import './SmellPage.css';
-import { Smell } from "../../interfaces/Smell";
+import {Smell, UrgencyCode} from "../../interfaces/Smell";
 import { useAnalysis } from '../../contexts/AnalysisContext';
+import {Analysis} from "../../interfaces/Analysis";
 
 const SmellPage = () => {
     const { analysisId, smellId } = useParams<{ analysisId: string, smellId: string }>();
-    const { getSmellById } = useAnalysis();
     const [smell, setSmell] = useState<Smell | undefined>();
+    const [analysis, setAnalysis] = useState<Analysis | undefined>();
+    const { getSmellById, fetchAnalysisById } = useAnalysis();
+    const navigate = useNavigate();
 
     useEffect(() => {
         console.log("Check Params:", analysisId, smellId);
         if (analysisId && smellId) {
+            fetchAnalysisById(parseInt(analysisId)).then(setAnalysis);
             console.log("Fetching smell for Analysis ID:");
             const fetchedSmell = getSmellById(parseInt(analysisId), parseInt(smellId));
             console.log("Fetched Smell:", fetchedSmell);
@@ -21,24 +25,76 @@ const SmellPage = () => {
         }
     }, [analysisId, smellId, getSmellById]);
 
+    const getUrgencyClass = (code: UrgencyCode | undefined) => {
+        return code ? `urgency-indicator ${code}` : 'urgency-indicator'; // Append the urgency code as a class
+    };
+
+    const handleSmellClick = (analysisId: number, smellId: number) => {
+        console.log("check params:", analysisId, smellId);
+        navigate(`/analysis/${analysisId}/smell/${smellId}`);
+    };
+
     return (
         <>
-            <TopBar />
-            <div className="smellPage-pageContainer">
-                <div className="smellPage-smellRefactor-container">
-                    <div className="smellPage-smellK">
-                        <h2 className="smellPage-smellName">{smell?.name } - {smell?.extendedName}</h2>
+            <TopBar/>
+            <div className="smellPage-header">
+                <h1 className="smellPage-analysisName">Analysis - {analysis?.name}</h1>
+                <div className={getUrgencyClass(smell?.urgencyCode)}></div>
+            </div>
+            <hr className="smellPage-headerSeparator"/>
+
+            <div className="smellPage-layout">
+                <aside className="smellPage-sidebar">
+                    <div className="smellPage-sidebarContent">
+                        <h3 className="smellPage-smellIndex">Smell: {smell?.id} / {analysis?.smells.length}</h3>
+                        {analysis?.smells.map((smell) => (
+                            <div key={smell.id} className="smellPage-smellListCard" onClick={() => handleSmellClick(analysis?.id, smell.id)}>
+                                <h3 className="smellPage-smellCardTitle">{smell.name}</h3>
+                                <p className="smellPage-smellCardMicroservice">{smell.microservice?.name}</p>
+                                <div className={getUrgencyClass(smell?.urgencyCode)}></div>
+                            </div>
+                        ))}
+                    </div>
+                </aside>
+
+                <main className="smellPage-mainContent">
+                    <h1 className="smellPage-smellTitle">{smell?.name} - {smell?.extendedName}</h1>
+                    <div id="analysis-output" className="smellPage-smellK">
+                        <h2 className="smellPage-smellName">Analysis Output - {smell?.name} </h2>
                         <p className="smellPage-smellDescription">{smell?.description}</p>
                     </div>
-                    <div className="smellPage-refactoring">
+                    <div id="refactoring" className="smellPage-refactoring">
                         <h3 className="smellPage-refactorName">Refactoring - {smell?.refactoring.name}</h3>
                         <p className="smellPage-refactorDescription">{smell?.refactoring.refactor}</p>
                     </div>
-                </div>
-                <div className="smellPage-smellDefinition">
-                    <h3 className="smellPage-smellDefinitionTitle">Smell Definition - {smell?.name }</h3>
-                    <p className="smellPage-smellDefinitionDescription">{smell?.smellTypeDescription}</p>
-                </div>
+                    <div id="smell-description" className="smellPage-smellDefinition">
+                        <h3 className="smellPage-smellDefinitionTitle">Smell Description - {smell?.extendedName}</h3>
+                        <p className="smellPage-smellDefinitionDescription">{smell?.smellTypeDescription}</p>
+                    </div>
+                    {/*
+                    <div className="smellPage-properties">
+                        <div className="smellPage-smellImpact">
+                            <h3 className="smellPage-propertiesTitle">Smell Impact</h3>
+                            {smell?.refactoring.propertiesAffected.map((attribute) => (
+                                <div key={attribute.name} className="smellPage-property">
+                                    <span
+                                        className={`smellPage-impactIndicator ${attribute.impactsPositively ? 'positive' : 'negative'}`}></span>
+                                    <span className="smellPage-propertyName">{attribute.name}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    */}
+                </main>
+
+                <nav className="smellPage-toc">
+                    <h3>Table of Contents</h3>
+                    <ul>
+                        <li><a href="#analysis-output">Analysis Output</a></li>
+                        <li><a href="#refactoring">Refactoring</a></li>
+                        <li><a href="#smell-description">Smell Description</a></li>
+                    </ul>
+                </nav>
             </div>
         </>
     );
