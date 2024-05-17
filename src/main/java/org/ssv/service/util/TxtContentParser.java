f04e574c384f47c46d235089bea3c9b13eea6536
Roberto Negro <r.negro3@campus.unimib.it>
2024-05-17 12:27:11 +0200

    Fixed txt parser for multiple smell situation

--- src/main/java/org/ssv/service/util/TxtContentParser.java
+++ src/main/java/org/ssv/service/util/TxtContentParser.java
@@ -9,6 +9,7 @@ import org.ssv.model.Smell;
 import org.ssv.model.SmellStatus;
 import org.ssv.service.FactoryAnalysis;
 import org.ssv.service.SmellDetail;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -18,36 +19,42 @@ public class TxtContentParser extends ContentParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(TxtContentParser.class);
 
     @Override
-    public List<Smell> parseContent(String content, Analysis analysis) throws InvalidContentException{
-        if(!Pattern.compile("^Analysis results:\\s*\n").matcher(content).find())
+    public List<Smell> parseContent(String content, Analysis analysis) throws InvalidContentException {
+        if (!Pattern.compile("^Analysis results:\\s*\n").matcher(content).find())
             throw new InvalidContentException("Invalid content");
 
         content = content.replaceAll("^Analysis results:\\s*\n", "");
         List<Smell> smells = new ArrayList<>();
 
-        Pattern pattern = Pattern.compile("\\{(.*?)\\}\\n(.*?)(?=\\n\\n|\\Z)", Pattern.DOTALL);
+        Pattern pattern = Pattern.compile("(.*?)\\s*-\\s*detected smells \\{(.*?)\\}\\n(.*?)(?=\\n\\n|\\Z)", Pattern.DOTALL);
         Matcher matcher = pattern.matcher(content);
 
         int i = 0;
         while (matcher.find()) {
-            String code = matcher.group(1).trim();
-            SmellDetail detail = FactoryAnalysis.getInstance().findSmellDetailByCode(code);
-            String description = matcher.group(2).trim();
-            Refactoring refactoring = assignTemplateValues(code, description, detail.getRefactoring());
-            System.out.println(code + " " + ++i);
+            String analysisValue = matcher.group(1).trim();
+            String codes = matcher.group(2).trim();
+            String description = matcher.group(3).trim();
 
-            Smell newSmell = Smell.builder()
-                    .code(code)
-                    .id(++i)
-                    .description(description)
-                    .extendedName(detail.getExtendedName())
-                    .propertiesAffected(detail.getPropertiesAffected())
-                    .refactoring(refactoring)
-                    .status(SmellStatus.UNFIXED)
-                    .analysis(analysis)
-                    .analysisId(analysis.getId())
-                    .build();
-            smells.add(newSmell);
+            // Gestire i codici smell concatenati
+            for (String code : codes.split(",")) {
+                code = code.trim();
+                SmellDetail detail = FactoryAnalysis.getInstance().findSmellDetailByCode(code);
+                Refactoring refactoring = assignTemplateValues(code, description, detail.getRefactoring());
+
+                Smell newSmell = Smell.builder()
+                        .code(code)
+                        .id(++i)
+                        .description(description)
+                        .extendedName(detail.getExtendedName())
+                        .propertiesAffected(detail.getPropertiesAffected())
+                        .refactoring(refactoring)
+                        .status(SmellStatus.UNFIXED)
+                        .analysis(analysis)
+                        .analysisId(analysis.getId())
+                        .outputAnalysis(analysisValue)
+                        .build();
+                smells.add(newSmell);
+            }
         }
         return smells;
     }
